08516c5a30f81d749b51da201b35355c74aa14ee
larusion <larusion@users.noreply.github.com>
2016-07-13 16:19:44 +0300

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,41 @@
+#ui.R
+
+library(shiny)
+
+
+
+shinyUI(
+        
+        pageWithSidebar(
+                
+        headerPanel("Olympic Track Times from 1896 to 1964"),
+        
+        sidebarPanel(
+                
+                tags$h2("Description"),
+                
+                tags$h5("This app displays the olympic track times in tenths of a second versus over the years that they were recorded, i.e., from 1896 to 1964. The user needs to choose the meters that the athletes ran. Click the Plot button to plot. If you would like to download, please click the Download button."),
+                
+                tags$h5("This dataset comes from Package 'cluster.datasets' of CRAN. References : Hartigan, J. A. (1975). Clustering Algorithms, John Wiley, New York."),
+                
+                
+                radioButtons("meters", label = h3("Choose Meters"),
+                             choices = c("100 m", "200 m", "400 m", "800 m", "1500 m", "5000 m", "10000 m")),
+                             
+          
+        
+                submitButton("Plot", icon = icon("bar-chart-o")),
+                
+                tags$br(),
+                
+                downloadButton('downloadData', 'Download')
+                
+        ),
+        
+        mainPanel(
+                
+                plotOutput("value")
+        )
+)
+)
+
